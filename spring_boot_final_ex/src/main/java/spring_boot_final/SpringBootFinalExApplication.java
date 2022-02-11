b4a0ce2473f34c147976bf430c8c682605b3d78c
Jinhwan <46812766+Midnight8@users.noreply.github.com>
2022-02-11 13:42:47 +0900

    start line
    초기 설정 완료

---
+++ spring_boot_final_ex/src/main/java/spring_boot_final/SpringBootFinalExApplication.java
@@ -0,0 +1,13 @@
+package spring_boot_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFinalExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFinalExApplication.class, args);
+	}
+
+}
